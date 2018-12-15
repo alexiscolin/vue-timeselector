8b847b2af7f2051e22d5aa630c5badd0f4185117
Alexis Colin <nesquimo@gmail.com>
2018-12-15 02:19:10 +0100

    Add interval on time

--- README.md
+++ README.md
@@ -10,3 +10,9 @@
 | displaySeconds                | Boolean         | false       | Display seconds to the input             |
 | separator                     | String          | :           | Separator symbol                         |
 | padTime                       | Boolean         | true        | Pads number with a zero                  |
+| format                        | Function        |             |                                          |
+| h24                           | Boolean         |             |                                          |
+| inline                        | Boolean         |             |                                          |
+| hoursInterval                 | Number|function | 1           |                                          |
+| minutesInterval               | Number|function | 10          |                                          |
+| secondsInterval               | Number|function | 10          |                                          |
--- src/VueTimeSelector.vue
+++ src/VueTimeSelector.vue
@@ -3,16 +3,13 @@
     <input type="text" readonly="readonly" autocomplete="off" v-model="timeToDisplay">
     <div class="vtimeselctor__box">
         <ul>
-          <li>1</li>
-          <li>2</li>
-          <li>3</li>
-          <li>4</li>
+          <li v-for="(hour, index) in timeCount(hoursInterval, hoursLength)" :key="index">{{hour}}</li>
         </ul>
         <ul>
-          <li>1</li>
-          <li>2</li>
-          <li>3</li>
-          <li>4</li>
+          <li v-for="(min, index) in timeCount(minutesInterval)" :key="index">{{min}}</li>
+        </ul>
+        <ul>
+          <li v-for="(sec, index) in timeCount(secondsInterval)" :key="index">{{sec}}</li>
         </ul>
     </div>
   </div>
@@ -41,6 +38,22 @@ export default {
     padTime: {
       type: Boolean,
       default: false
+    },
+    h24: {
+      type: Boolean,
+      default: false
+    },
+    hoursInterval: {
+      type: Number,
+      default: 1
+    },
+    minutesInterval: {
+      type: Number,
+      default: 10
+    },
+    secondsInterval: {
+      type: Number,
+      default: 10
     }
   },
   data () {
@@ -48,11 +61,19 @@ export default {
       picker: {
         hour: 0,
         min: 0,
-        sec: 0
-      }
+        sec: 0,
+        long: false
+      },
+      longHourCount: 24,
+      shortHourCount: 12,
+      minCount: 60,
+      secCount: 60
     }
   },
   computed: {
+    hoursLength () {
+      return this.h24 ? this.longHourCount : this.shortHourCount;
+    },
     timeToDisplay () {
       return  (this.displayHours ? (this.pad(this.picker.hour)) : '') +
               (this.displayMinutes && this.displayHours ? (this.separator + this.pad(this.picker.min)) : (this.displayMinutes ? this.padTime(this.picker.min) : '')) +
@@ -62,6 +83,9 @@ export default {
   methods: {
     pad (time) {
       return this.padTime ? time.toString().padStart(2, '0') : time;
+    },
+    timeCount (interval = 1, timeLength = 60) {
+      return Array.apply(null, {length: timeLength}).map(Number.call, Number => Number + 1).filter(num => num % interval === 0);
     },
   }
 }
