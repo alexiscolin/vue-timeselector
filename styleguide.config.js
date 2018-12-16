db1c567a1f03ed7a001fbe742757c9a40f95e455
Alexis Colin <nesquimo@gmail.com>
2018-12-16 19:07:47 +0100

    Change doc dir

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,6 +1,7 @@
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 module.exports = {
   components: 'src/**/[A-Z]*.vue',
+  styleguideDir: 'docs',
   webpackConfig: {
     module: {
       rules: [
