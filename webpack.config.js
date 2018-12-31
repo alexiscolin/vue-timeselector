2512cdfff7fbd6e7ec8b65c5f8ea294af8968b6e
Alexis Colin <nesquimo@gmail.com>
2018-12-31 15:41:32 +0100

    Add common js esm for vue in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,11 @@ module.exports = {
       }
     ]
   },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+  },
   plugins: [
     new VueLoaderPlugin(),
     new HtmlWebPackPlugin({
